3ccda2356142c723d00d014f36d1c5aa49d9265a
Давид Абрамов <128404076+DavidoffAbramoff@users.noreply.github.com>
2023-07-03 18:59:23 +0300

    Create work.java

---
+++ work.java
@@ -0,0 +1,13 @@
+public class work {
+    public static void main(String args[]){
+     int n = 7;
+      if((n%2)==0){
+            System.out.println("Число " + n + " четное ");
+        }
+        else{
+           if((n%1)==0){
+             System.out.println("Число " + n + " нечетное ");
+           }
+        }
+    }
+}
